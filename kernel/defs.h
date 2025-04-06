490a319e6fc8a5da559994d700474b297edbddc9
Nick Chandler <nwchandler@users.noreply.github.com>
2025-04-05 22:50:15 -0400

    Add syscall to get free memory

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,6 +63,7 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+uint            kfreesize();
 
 // log.c
 void            initlog(int, struct superblock*);
